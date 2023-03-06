7d106b463b1e62f2e3b193dca7b6f69e9c959825
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-06 17:02:59 +0000

    add cookies for all calculations

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -35,6 +35,10 @@ class CalculationsController < ApplicationController
 
     @result =  @second_number.to_f - @first_number.to_f
 
+    cookies.store(:substraction_first, @first_number)
+    cookies.store(:substraction_second, @second_number)
+    cookies.store(:substraction_result, @result)
+
     render({ :template => "calculation_templates/subtract_results.html.erb" })
   end
 
@@ -44,6 +48,10 @@ class CalculationsController < ApplicationController
 
     @result =  @first_number.to_f * @second_number.to_f
 
+    cookies.store(:multiplication_first, @first_number)
+    cookies.store(:multiplication_second, @second_number)
+    cookies.store(:multiplication_result, @result)
+
     render({ :template => "calculation_templates/multiply_results.html.erb" })
   end
 
@@ -52,6 +60,10 @@ class CalculationsController < ApplicationController
     @second_number = params.fetch("second_num").to_f
 
     @result =  @first_number.to_f / @second_number.to_f
+
+    cookies.store(:division_first, @first_number)
+    cookies.store(:division_second, @second_number)
+    cookies.store(:division_result, @result)
 
     render({ :template => "calculation_templates/divide_results.html.erb" })
   end
